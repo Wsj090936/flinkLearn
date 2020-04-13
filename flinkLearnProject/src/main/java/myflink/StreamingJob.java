8530cc3031f0222421106f467024e454799fd8f4
wushijia <jiahao@mogu.com>
2020-04-13 22:13:48 +0800

    ignore

--- flinkLearnProject/src/main/java/myflink/StreamingJob.java
+++ flinkLearnProject/src/main/java/myflink/StreamingJob.java
@@ -22,6 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -63,6 +64,7 @@ public class StreamingJob {
 		});
 		SingleOutputStreamOperator<Tuple2<String, Integer>> sum = streamOperator.keyBy(0).timeWindow(Time.seconds(5)).sum(1);
 		sum.print();
+		sum.writeAsCsv("D:\\flink\\flinkLearnProject\\src\\main\\resources\\testfile", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
 		// execute program
 		env.execute("stream demo");
 	}
